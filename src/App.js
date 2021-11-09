de5249a93c9cf14f6ca05b6b7232507bce7ff573
Grzesiek <cokaco88@gmail.com>
2021-11-09 12:47:44 +0100

    workflow changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <h2>New Update Again with CI/CD</h2>
+          <h2>New Update Again with CI/CD 2</h2>
         </p>
         <a
           className="App-link"
